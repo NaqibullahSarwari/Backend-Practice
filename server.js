8fa31bfb226a55ca4bd4784be267a80e9d087aa8
Naqibullah <info.naqibullahsarwari@gmail.com>
2025-10-25 16:59:27 +0500

    Project Setup Completed

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
+import connectDB from "./mongoose.js";
 import productRoutes from "./Routes/ProductRoutes.js";
 
 dotenv.config();
 
 const server = express();
+
+connectDB();
 
 server.use(cors());
 server.use(bodyParser.json());
